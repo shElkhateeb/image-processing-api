c817804ab8525fc1cfa4d068ac789d8fb9d1b517
Shorouk Elkhateeb <shrouca@gmail.com>
2022-03-22 19:05:21 +0200

    feat: Check if the resized image exsisted before processing

--- src/utilities/resizeimage.ts
+++ src/utilities/resizeimage.ts
@@ -1,17 +1,44 @@
 import express from 'express';
-import sharp from "sharp";
+import path from 'path';
+import sharp from 'sharp';
 import { promises as fsPromises } from 'fs';
 
-const resizeImage = (req: express.Request, res: express.Response, next: Function): void=>{
-    const filename = (req.query.filename as unknown) as string;
-    const width = (req.query.width as unknown) as number;
-    const height = (req.query.height as unknown) as number;
-    console.log(filename);
-    // if resized image not found in thumbnail folder
+const resizeImage = async (
+  filename: string,
+  width: number,
+  height: number,
+  imgPath: string
+): Promise<void> => {
+  console.log(filename);
+  // check if resized image not found in thumbnail folder
+  if (await exists(path.resolve(imgPath))) {
+      console.log('found');
+    return;
+  } else {
+    // open image file
+    console.log('processing image');
+    try {
+      const originalImage = fsPromises.readFile(
+        path.resolve('Images/' + filename + '.jpg')
+      );
+      console.log(originalImage);
+    } catch (err) {
+      console.log(err);
+    }
     // resize image
-    // save image in thumbnail folder
     
-    next();
+    // save image in thumbnail folder
+  }
+};
+
+// function to check if a file exists
+async function exists(path: string) {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 export default resizeImage;
